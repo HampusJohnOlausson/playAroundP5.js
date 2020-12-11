6249a9c2c33fb587e6535af37a39967e65e5382d
Hampus Olausson <hampus.john.olausson@gmail.com>
2020-12-11 17:03:11 +0100

    just testing

--- scetch.js
+++ scetch.js
@@ -1,49 +1,34 @@
-let x = 0; 
-let y = 0;
-let speed = 3; 
+class Game {
 
-let on = false;
+    private menu: Menu;
+    private gameController: GameController;
+    private isGameRunning: Boolean;
 
-function setup(){
-
-    createCanvas(600, 400);
-}
-
-function draw(){
-
-    background(0,191,255);
-    
-   
-    fill('green');
-    ellipse(x, y, 100, 100);
-    
-    // appleOne = new Apple(x, 40, 30, 30);
-    // appleOne.shape();
-   
-
-    if( x > width){
-        speed = -3;
+    constructor(){
+        this.menu = new Menu();
+        this.gameController = new GameController();
+        this.isGameRunning = false;
     }
-    x = x + speed;
-    y = y + speed;
-
-    rect(mouseX, 300, 100, 100)
-    noCursor();
-    
+    public update(){}
+    public draw(){}
 }
 
-// class Apple {
-//     constructor(x, y, d){
-//         this.x = x,
-//         this.y = y,
-//         this.d = d
-//     }
-//     shape(){
-//         fill(220,20,60)
-//         ellipse(this.x, this.y, this.y * 2);
-//     }
-//     move(){
-//         this.y = this.y + 6;
-//         drop2.y = drop2.y + 2;
-//     }
-// }
+class Menu {
+
+    constructor(){}
+    public update(){
+        if(this.isGameRunning) {
+            this.gameController.draw();
+        } else {
+            this.menu.draw();
+        }
+    }
+    public draw(){}
+}
+
+class GameController {
+
+    constructor(){}
+    public update(){}
+    public draw(){}
+}
